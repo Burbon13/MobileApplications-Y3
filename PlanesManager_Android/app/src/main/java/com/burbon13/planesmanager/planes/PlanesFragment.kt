2b6f0f1da3866e60d94f8b118992ff1587dae8d4
Razvan <razvan_roatis@yahoo.com>
2019-12-26 23:27:52 +0200

    Added 'Add plane' button

--- PlanesManager_Android/app/src/main/java/com/burbon13/planesmanager/planes/PlanesFragment.kt
+++ PlanesManager_Android/app/src/main/java/com/burbon13/planesmanager/planes/PlanesFragment.kt
@@ -5,10 +5,10 @@ import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.burbon13.planesmanager.R
 import com.burbon13.planesmanager.core.TAG
 import com.burbon13.planesmanager.core.utils.extensions.hideKeyboard
@@ -16,6 +16,7 @@ import com.burbon13.planesmanager.core.utils.extensions.setDivider
 
 import com.burbon13.planesmanager.planes.dummy.DummyContent
 import com.burbon13.planesmanager.planes.model.Plane
+import kotlinx.android.synthetic.main.fragment_plane_list.*
 
 
 /**
@@ -40,21 +41,17 @@ class PlanesFragment : Fragment(), OnListFragmentInteractionListener {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_plane_list, container, false)
 
-        // Set the adapter
-        if (view is RecyclerView) {
-            view.setDivider(R.drawable.recycler_view_divider)
-            with(view) {
-                layoutManager = when {
-                    columnCount <= 1 -> LinearLayoutManager(context)
-                    else -> GridLayoutManager(context, columnCount)
-                }
-                adapter =
-                    MyPlaneRecyclerViewAdapter(
-                        DummyContent.ITEMS,
-                        this@PlanesFragment as OnListFragmentInteractionListener
-                    )
-            }
+        val recyclerViewPlanes = view.findViewById<RecyclerView>(R.id.recycler_view_planes)
+        recyclerViewPlanes.setDivider(R.drawable.recycler_view_divider)
+        recyclerViewPlanes.layoutManager = when {
+            columnCount <= 1 -> LinearLayoutManager(context)
+            else -> GridLayoutManager(context, columnCount)
         }
+        recyclerViewPlanes.adapter = MyPlaneRecyclerViewAdapter(
+            DummyContent.ITEMS,
+            this@PlanesFragment as OnListFragmentInteractionListener
+        )
+
         return view
     }
 
