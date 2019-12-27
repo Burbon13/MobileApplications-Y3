3fb918a16e875b80a66b553ba28774b6d7246bc3
Razvan <razvan_roatis@yahoo.com>
2019-12-27 23:39:44 +0200

    Added NeDB-promises node package & implemented pagination get for planes

--- PlanesManager_Server_Node/src/plane/router.js
+++ PlanesManager_Server_Node/src/plane/router.js
@@ -20,6 +20,13 @@ router.get('/:id', async (ctx) => {
     }
 });
 
+router.get('/page/:pageNumber', async (ctx) => {
+    const response = ctx.response;
+    const pageNumber = ctx.params.pageNumber;
+    response.body = await planeStore.findPage(pageNumber);
+    response.status = 200;
+});
+
 const createPlane = async (plane, response) => {
     try {
         response.body = await planeStore.insert(plane);
@@ -61,3 +68,36 @@ router.del('/:id', async (ctx) => {
     await planeStore.remove({_id: ctx.params.id});
     ctx.response.status = 204;
 });
+
+router.post('/generate/:nr', async (ctx) => await generatePlanes(ctx.params.nr, ctx.response));
+
+
+/**
+ * Generates new planes and deletes the old ones
+ *
+ * @param nrPlanes the number of new planes
+ */
+const generatePlanes = async (nrPlanes, response) => {
+    console.log('Deleting all planes ... :(');
+    await planeStore.remove({});
+    console.log('Generating new planes ... :)');
+
+    try {
+        for (let i = 1; i <= nrPlanes; i++) {
+            let newPlane = {
+                "tailNumber": "KLM-" + i,
+                "brand": "Boeing",
+                "model": "747-800",
+                "fabricationYear": 2002,
+                "engine": "TURBOJET",
+                "price": 3431000505
+            };
+            await planeStore.insert(newPlane);
+        }
+        response.body = 'Success!';
+        response.status = 201;
+    } catch (err) {
+        response.body = {issue: [{error: err.message}]};
+        response.status = 400;
+    }
+};
--- PlanesManager_Server_Node/src/plane/store.js
+++ PlanesManager_Server_Node/src/plane/store.js
@@ -1,8 +1,9 @@
-import dataStore from 'nedb-promise';
+const Datastore = require('nedb-promises');
 
 export class PlaneStore {
-    constructor({filename, autoload}) {
-        this.store = dataStore({filename, autoload});
+    constructor(filename) {
+        this.store = Datastore.create(filename);
+        this.pageSize = 15;
     }
 
     async find(props) {
@@ -13,30 +14,41 @@ export class PlaneStore {
         return this.store.findOne(props);
     }
 
+    /**
+     * Queries the planes on a certain page
+     *
+     * @param pageNumber the index of the desired page (first page = 0)
+     * @returns {Promise<*>}
+     */
+    async findPage(pageNumber) {
+        pageNumber -= 1;
+        return this.store.find({}).sort({tailNumber: 1}).skip((pageNumber - 1) * this.pageSize).limit(this.pageSize);
+    }
+
     async insert(plane) {
         let tailNumber = plane.tailNumber;
         if (!tailNumber) {
-            throw new Error('Missing tail_number property')
+            throw new Error('Missing tail_number property');
         }
         let brand = plane.brand;
         if (!brand) {
-            throw new Error('Missing brand property')
+            throw new Error('Missing brand property');
         }
         let model = plane.model;
         if (!model) {
-            throw new Error('Missing model property')
+            throw new Error('Missing model property');
         }
         let fabricationYear = plane.fabricationYear;
         if (!fabricationYear) {
-            throw new Error('Missing fabrication_year property')
+            throw new Error('Missing fabrication_year property');
         }
         let engine = plane.engine;
         if (!engine) {
-            throw new Error('Missing engine property')
+            throw new Error('Missing engine property');
         }
         let price = plane.price;
         if (!price) {
-            throw new Error('Missing price property')
+            throw new Error('Missing price property');
         }
         return this.store.insert(plane);
     };
