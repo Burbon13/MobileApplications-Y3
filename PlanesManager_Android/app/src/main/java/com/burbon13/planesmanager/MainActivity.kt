e6b32e7dff87b8b041e6f2b55b439d6ebcd90836
Razvan <razvan_roatis@yahoo.com>
2019-12-29 14:52:34 +0200

    Solved back button bug

--- PlanesManager_Android/app/src/main/java/com/burbon13/planesmanager/MainActivity.kt
+++ PlanesManager_Android/app/src/main/java/com/burbon13/planesmanager/MainActivity.kt
@@ -2,9 +2,13 @@ package com.burbon13.planesmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.burbon13.planesmanager.core.TAG
 import kotlinx.android.synthetic.main.activity_navigation.*
 
 
@@ -15,14 +19,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
         navController = findNavController(R.id.my_nav_host_fragment)
-        NavigationUI.setupActionBarWithNavController(
-            this,
-            navController
+        setupActionBarWithNavController(
+            navController, AppBarConfiguration(
+                setOf(
+                    R.id.planesFragment,
+                    R.id.loginFragment
+                )
+            )
         )
         NavigationUI.setupWithNavController(
             nav_view,
             navController
         )
+        supportFragmentManager.addOnBackStackChangedListener {
+            Log.d(TAG, "addOnBackStackChangedListener() called ...")
+            if (supportFragmentManager.backStackEntryCount > 0) {
+                supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            } else {
+                supportActionBar?.setDisplayHomeAsUpEnabled(false)
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
