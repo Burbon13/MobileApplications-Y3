57ac55cc28bbb57bbd3d6706d3ab2498b2be5b69
Razvan <razvan_roatis@yahoo.com>
2019-12-30 19:00:50 +0200

    Created plane details fragment

---
+++ PlanesManager_Android/app/src/main/java/com/burbon13/planesmanager/planes/ui/plane/PlaneDataFragment.kt
@@ -0,0 +1,24 @@
+package com.burbon13.planesmanager.planes.ui.plane
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class PlaneDataFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_plane_data, container, false)
+    }
+
+
+}
