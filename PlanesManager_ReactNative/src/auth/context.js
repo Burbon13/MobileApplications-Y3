ae94cb7ab907298be99b7c44f5c855e96ea5b0af
Burbon13 <razvanroatis91@gmail.com>
2020-01-05 23:41:37 +0200

    Solved nasty bug

--- PlanesManager_ReactNative/src/auth/context.js
+++ PlanesManager_ReactNative/src/auth/context.js
@@ -3,6 +3,6 @@ import React from 'react';
 export const AuthContext = React.createContext({});
 
 const AuthContextProvider = AuthContext.Provider;
-const AuthContextConsumer = AuthContext.Provider;
+const AuthContextConsumer = AuthContext.Consumer;
 
 export {AuthContextProvider, AuthContextConsumer};
